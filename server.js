be618e6e3b9988961f5c8c3b9b5920952bd92b8a
zoharehan <64752194+zoharehan@users.noreply.github.com>
2022-03-18 20:33:40 -0400

    change route

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 	//res.send('This should be the root route!')
 
 	//sending some HTML
-	res.send('<h1>This should be the root route!</h1>')
+	res.sendFile('/pub/examples.html');
 })
 
 // Error codes
